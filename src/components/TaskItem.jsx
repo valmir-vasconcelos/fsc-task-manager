d7eb86da78828dfd4a5da4cd3cc7999fa6af53c1
Valmir Vasconcelos de Araujo <valmir.vasconcelos@parauapebas.pa.gov.br>
2025-01-07 12:10:28 -0300

    Adicionado componente TaskItem

---
+++ src/components/TaskItem.jsx
@@ -0,0 +1,43 @@
+import CheckIcon from "../assets/icons/check.svg?react";
+import DetailsIcon from "../assets/icons/details.svg?react";
+import LoaderIcon from "../assets/icons/loader.svg?react";
+
+const TaskItem = ({ task }) => {
+  const getStatusClasses = () => {
+    if (task.status === "done") {
+      return "bg-[#00ADB5] text-[#00ADB5]";
+    } else if (task.status === "in_progress") {
+      return "bg-[#FFAA04] text-[#FFAA04]";
+    } else if (task.status === "not_started") {
+      return "bg-[#35383E] bg-opacity-10 text-[#35383E]";
+    }
+  };
+
+  return (
+    <div
+      className={`flex justify-between items-center gap-2 bg-opacity-10 rounded-lg px-4 py-3 ${getStatusClasses()}`}
+    >
+      <div className="flex items-center gap-2">
+        <label
+          className={`relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg ${getStatusClasses()}`}
+        >
+          <input
+            type="checkbox"
+            checked={task.status === "done"}
+            className="absolute h-full w-full cursor-pointer opacity-0"
+          />
+          {task.status === "done" && <CheckIcon />}
+          {task.status === "in_progress" && (
+            <LoaderIcon className="animate-spin text-white" />
+          )}
+        </label>
+        {task.title}
+      </div>
+      <a href="#" className="transition hover:opacity-75">
+        <DetailsIcon />
+      </a>
+    </div>
+  );
+};
+
+export default TaskItem;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,12 +1,21 @@
-import Button from "./Button";
+import { useState } from "react";
 import AddIcon from "../assets/icons/add.svg?react";
-import TrashIcon from "../assets/icons/trash.svg?react";
-import SunIcon from "../assets/icons/sun.svg?react";
 import CloudSunIcon from "../assets/icons/cloud-sun.svg?react";
 import MoonIcon from "../assets/icons/moon.svg?react";
+import SunIcon from "../assets/icons/sun.svg?react";
+import TrashIcon from "../assets/icons/trash.svg?react";
+import TASKS from "../service/dados";
+import Button from "./Button";
+import TaskItem from "./TaskItem";
 import TaskSeparator from "./TaskSeparator";
 
 const Tasks = () => {
+  const [tasks] = useState(TASKS);
+
+  const morningTasks = tasks.filter((task) => task.time === "morning");
+  const afternoonTasks = tasks.filter((task) => task.time === "afternoon");
+  const eveningTasks = tasks.filter((task) => task.time === "evening");
+
   return (
     <div className="w-full px-8 py-16">
       <div className="flex w-full justify-between">
@@ -32,12 +41,21 @@ const Tasks = () => {
       <div className="rounded-xl bg-white p-6">
         <div className="space-y-3">
           <TaskSeparator title="Manhã" icon={<SunIcon />} />
+          {morningTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
         </div>
         <div className="my-6 space-y-3">
           <TaskSeparator title="Tarde" icon={<CloudSunIcon />} />
+          {afternoonTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
         </div>
         <div className="space-y-3">
           <TaskSeparator title="Noite" icon={<MoonIcon />} />
+          {eveningTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
         </div>
       </div>
     </div>
